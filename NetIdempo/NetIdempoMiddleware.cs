11ae4c8e89d1d7655411b7fe4317b0efaf50cb7d
josedanielcr <jdcanales1905@gmail.com>
2025-05-20 21:46:16 -0600

    Feat: Main structure of middleware

---
+++ NetIdempo/NetIdempoMiddleware.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+namespace NetIdempo;
+
+public class NetIdempoMiddleware(RequestDelegate next)
+{
+    public async Task InvokeAsync(HttpContext context)
+    { 
+        Console.WriteLine($"[NetIdempo] Request started: {context.Request.Method} {context.Request.Path}"); 
+        await next(context);
+        Console.WriteLine($"[NetIdempo] Request ended: {context.Request.Method} {context.Request.Path}");
+    }
+}
---
+++ TestApi/Program.cs
@@ -0,0 +1,16 @@
+using NetIdempo;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+app.UseMiddleware<NetIdempoMiddleware>();
+app.Run();
